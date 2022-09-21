5f1f504861e207945e7f951129cae52d9762e309
Vincent Leclerc <vleclerc@inow.fr>
2022-09-21 10:38:12 +0200

    lazy loading & bundle

--- src/conf/routes.js
+++ src/conf/routes.js
@@ -1,14 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../components/views/HomeView.vue';
-import CarAddView from '../components/views/cars/CarAddView.vue';
-import CarListView from '../components/views/cars/CarListView.vue';
-import LoginView from '../components/views/authentication/LoginView.vue';
+
+
+//import CarAddView from '../components/views/cars/CarAddView.vue';
+//import CarListView from '../components/views/cars/CarListView.vue';
+const CarAddView = () => import('../components/views/cars/CarAddView.vue');
+const CarListView = () => import('../components/views/cars/CarListView.vue');
+
+//import LoginView from '../components/views/authentication/LoginView.vue';
+const LoginView = () => import('../components/views/authentication/LoginView.vue')
 
 const routes = [
     { path: '/', component: HomeView },
     { path: '/cars/add', component: CarAddView },
     { path: '/cars/list', component: CarListView },
     { path: '/login', component: LoginView }
+
 ];
 
 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'cars': [
+            './src/components/views/cars/CarAddView.vue',
+            './src/components/views/cars/CarListView.vue'
+          ]
+        }
+      }
+    }
+  }
 })
