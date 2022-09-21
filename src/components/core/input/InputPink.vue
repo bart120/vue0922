ab0b427281326efece1c30a18770abe1d0257f14
Vincent Leclerc <vleclerc@inow.fr>
2022-09-21 15:25:29 +0200

    attrs, props, computed

---
+++ src/components/core/input/InputPink.vue
@@ -0,0 +1,41 @@
+<script setup>
+import InputText from 'primevue/inputtext';
+</script>
+
+<script>
+
+export default {
+
+    /*data() {
+        console.log("data");
+        return {
+            displayColor: this.$attrs.modelValue == '' ? 'background-color: pink' : ''
+        }
+    },*///NO
+    props: {
+        label: String
+    },
+
+    mounted() {
+        //console.log("props", this.$props);
+        //console.log("attrs", this.$attrs);
+    },
+    updated() {
+        //console.log("updated");
+        //this.displayColor = this.$attrs.modelValue == '' ? 'background-color: pink' : '';//NO
+    },
+    computed: {
+        displayColor() {
+            return this.$attrs.modelValue == '' ? 'background-color: pink' : '';
+        }
+    }
+
+}
+</script>
+
+
+<template>
+    <label>{{label}}</label>
+    <InputText v-bind="this.$attrs" :style="displayColor" />
+
+</template>
--- src/components/views/authentication/LoginView.vue
+++ src/components/views/authentication/LoginView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
+import InputPink from '../../core/input/InputPink.vue'
 
 //composition
 /*function submit() {
@@ -31,12 +32,15 @@ export default {
 
     <form @submit.prevent="submit" novalidate>
         <div>
-            <label>Login</label>
-            <InputText type="email" v-model="user.login" />
+            <!--<label>Login</label>
+            <InputText type="email" v-model="user.login" />-->
+
+            <InputPink label="Login" type="email" v-model="user.login" />
         </div>
         <div>
-            <label>Mot de passe</label>
-            <InputText type="password" v-model="user.password" />
+            <!--<label>Mot de passe</label>
+            <InputText type="password" v-model="user.password" />-->
+            <InputPink label="Mot de passe" type="password" v-model="user.password" />
         </div>
         <div>
             <Button type="submit">Connexion</Button>
